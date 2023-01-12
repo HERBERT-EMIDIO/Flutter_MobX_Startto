a11ba94c9286f715450a2ce516657f66b454cd7d
Herbert Felipe <herbertemidio2017@gmail.com>
2023-01-12 14:45:52 -0300

    deixando o cod mais limpo_simples

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,16 +1,26 @@
 import 'package:mobx/mobx.dart';
 
-class Counter {
+part 'counter.g.dart';
 
-  //Estado que será modificado --> observable  //7.51
-  final _count = Observable(0);
+class Counter = CounterBase with _$Counter;
 
-  int get count => _count.value;
+abstract class CounterBase with Store {
+  //1. forma de fazer: Estado que será modificado --> observable
+  /*final _count = Observable(0);
+  int get count => _count.value;*/
 
-//modificar o estado --> ação = action increment
-  late Action increment = Action(_increment);
+  //2. forma de fazer: @Observer
+  @observable
+  int count = 0;
 
-  void _increment() {
-    _count.value++;
-  }
+//1. forma de modificar o estado --> ação = action increment
+  /*late Action increment = Action(_increment); 
+  void _increment(){
+  _count.value++;}*/
+
+  //2. forma de modificar com @action void increment(){count++;}
+  @action
+  void increment() => count++;
+  //{count++;}
+
 }
